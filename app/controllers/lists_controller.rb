8e80b67ea2da6e6a71c2d83351681f9eaf62df28
kakas <shrimptrain@gmail.com>
2016-03-03 00:03:35 +0800

    新增、刪除、修改 待辦清單

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,46 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+    @new_list = List.new
+  end
+
+  def new
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to lists_path
+    else
+      render :back
+    end
+  end
+
+  def update
+    @list = List.find(params[:id])
+
+    if @list.update(list_params)
+      redirect_to lists_path
+    else
+      render :back
+    end
+
+
+  end
+
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path
+  end
+
+
+  private
+
+  def list_params
+    params.require(:list).permit(:content, :done)
+  end
+
+end
