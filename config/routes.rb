58410f753da41ebbc7e037c5b18ba7e4f9c47bb5
kakas <shrimptrain@gmail.com>
2016-03-03 01:50:43 +0800

    完成三種顯示 全部 未完成 已完成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
     member do
       post :done_toggle
     end
+
+    collection do
+      get :visible_all
+      get :visible_active
+      get :visible_complete
+    end
   end
 
 end
