698356276db32b9b0cc42852f3521e1ac0e58611
kakas <shrimptrain@gmail.com>
2016-03-10 22:04:49 +0800

    判斷目前board tab重構成helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_board
+
   def current_board
     @current_board ||= find_current_board
   end
--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -1,2 +1,9 @@
 module BoardsHelper
+
+  def nav_tab_active_or_not(board)
+    if board == current_board
+      "active"
+    end
+  end
+
 end
